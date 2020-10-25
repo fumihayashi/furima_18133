19f967249a42806c1776c01d541fc0c0248eb64a
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-10-25 21:52:17 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    # バリデーションで問題があれば、保存はされず「商品出品画面」を再描画
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    end
+    # アクションのnewをコールすると、エラーメッセージが入った@itemが上書きされてしまうので注意
+    render 'new'
+  end
 end
